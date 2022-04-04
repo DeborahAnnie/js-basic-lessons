d556d15bf21e3e562c01b8d357024dd94e1b4edf
Annie Deborah J <anni3014@global.chainsys.com>
2022-04-04 09:04:04 +0530

    fourth commit

--- ng-workspace/demob-app/src/app/app.component.ts
+++ ng-workspace/demob-app/src/app/app.component.ts
@@ -9,31 +9,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   fg = new FormGroup({
     fname: new FormControl('', [Validators.required]),
-    lname: new FormControl('', [Validators.required]),
+    aadhar: new FormControl('', [Validators.required]),
     dob: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
     city: new FormControl('', [Validators.required]),
     date: new FormControl('', [Validators.required]),
     time: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
     lab: new FormControl('', [Validators.required]),
     report: new FormControl('', [Validators.required]),
     result: new FormControl('', [Validators.required]),
-    aadhar: new FormControl('', [Validators.required]),
   });
   store: any = [];
   input: any;
   headers = [
     'First Name',
-    'Last Name',
+    'Aadhar Number',
     'Date of Birth',
-    'Gender',
     'City',
     'Date Of Test',
     'Time Of Test',
+    'Gender',
     'Lab Name',
     'Report Number',
     'Result',
-    'Aadhar Number',
   ];
   title: any;
   searchvalue: any = [];
