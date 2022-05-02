cf8392a58f57a6709887c798c8c8517cb5ddccfd
Annie Deborah J <anni3014@global.chainsys.com>
2022-05-02 09:54:46 +0530

    first commit

--- ng-workspace/demoa-app/src/app/count-display/count-display.component.ts
+++ ng-workspace/demoa-app/src/app/count-display/count-display.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-count-display',
   templateUrl: './count-display.component.html',
-  styleUrls: ['./count-display.component.css']
+  styleUrls: ['./count-display.component.css'],
 })
 export class CountDisplayComponent implements OnInit {
- @Input()
- count: number = 0;
-  constructor() { }
+  @Input()
+  count: number = 0;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
